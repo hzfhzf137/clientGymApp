008fafeb52d1e5d7069bb504c6d195c446956486
hzfhzf137 <hzfhzf137@gmail.com>
2025-01-24 19:48:58 +0500

    update
    it

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// import aos
-import Aos from 'aos';
-import 'aos/dist/aos.css';
-
-// import components
-import { About, Banner, Footer, Header, Pricing, Workouts } from "./components";
+import React, { useEffect } from "react";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import About from "./components/About";
+import Workouts from "./components/Workouts";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
-  // aos initialization
-  Aos.init({
-    duration: 2500,
-    delay: 400,
-  });
+  useEffect(() => {
+    Aos.init({
+      duration: 2500,
+      delay: 400,
+    });
+  }, []);
 
   return (
     <div className="max-w-[1440px] mx-auto bg-page overflow-hidden relative">
